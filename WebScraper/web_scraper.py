05f999c53229bd8c0dac07fa08abdf81e3d42f40
b9022961 <b9022961@newcastle.ac.uk>
2020-12-10 11:47:38 +0000

    Completed the news feed with the addition of url link, output data is in form of a list with name 'correct_list'

--- WebScraper/web_scraper.py
+++ WebScraper/web_scraper.py
@@ -6,45 +6,58 @@ import requests
 import fileinput
 import wget
 import html2text
+import re
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import io
+from pprint import pprint
 
 
 def main():
     news_url = "https://www.ncl.ac.uk/press/latest/"
-    scrape_data(news_url)
+    news_file = "news_data.txt"
+    scrape_data(news_url, news_file)
 
 
-def read_news():
+def read_news(news_file, url_list):
     text_as_list = []
-    with io.open("news_data.txt", "r", encoding="utf-8") as f:
+    with io.open(news_file, "r", encoding="utf-8") as f:
         for line in f:
             text_as_list.append(line)
-    organise(text_as_list)
+    organise(text_as_list, url_list)
 
 
-def scrape_data(url):
+def scrape_data(url, news_file):
     html = urlopen(url).read()
     soup = BeautifulSoup(html, features="html.parser")
 
     for script in soup(["script", "style"]):
         script.extract()
-
     text = soup.get_text()
+    tags = soup.find_all("a")
+    url_list = []
+    correct_url_list = []
+    p = re.compile("/press/articles/latest*")
+    for tag in tags:
+        url_list.append(tag.get('href'))
+    for i in range(len(url_list)):
+        if p.match(url_list[i]):
+            url_list = url_list[i:i + 20]
+            break
+    for i in range(len(url_list)):
+        pre = "https://www.ncl.ac.uk" + url_list[i]
+        correct_url_list.append(pre)
 
     lines = (line.strip() for line in text.splitlines())
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
     text = '\n'.join(chunk for chunk in chunks if chunk)
 
-    with io.open("news_data.txt", "w", encoding="utf-8") as f:
+    with io.open(news_file, "w", encoding="utf-8") as f:
         f.write(text)
-    
-    read_news()
+    read_news(news_file, correct_url_list)
 
 
-def organise(text_as_list):
-    global new_list
+def organise(text_as_list, url_list):
     for i in range(len(text_as_list)):
         if text_as_list[i] == "Latest News\n":
             text_as_list = text_as_list[i + 1:i + 301]
@@ -61,7 +74,14 @@ def organise(text_as_list):
             correct_list.append(new_list)
             new_list = []
     correct_list = correct_list[0:20]
-    print(correct_list)
+    list3 = []
+    for j in range(len(correct_list)):
+        list2 = correct_list[j]
+        list2.append(url_list[j])
+        list3.append(list2)
+
+    correct_list = list3
+    print(len(correct_list))
 
 
 if __name__ == "__main__":
