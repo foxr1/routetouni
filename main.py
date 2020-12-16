89c7f5bcea374d2c971ab13895fa322feb7eadef
philipsolo <philipsolo4@gmail.com>
2020-12-16 12:02:44 +0200

    fixed bootstrap issues, chat works, TODO multiple channels

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import os
 import uuid
 import datetime
 import redis
-from flask import Flask, render_template, session, redirect, url_for, request, Blueprint, Response, send_from_directory
+from flask import Flask, render_template, session, redirect, url_for, request, send_from_directory
 from flask_socketio import emit, join_room, leave_room, SocketIO
-
 
 # Connect to redis on Docker
 # r = redis.Redis(host='localhost', port=6379, charset="utf-8", decode_responses=True)
 
 # Connect to redis on GCP
+from forms import LoginForm
 
 redis_host = os.environ.get('REDISHOST', 'localhost')
 redis_port = int(os.environ.get('REDISPORT', 6379))
@@ -53,13 +53,32 @@ def news_feed():
     return render_template("news_feed.html")
 
 
-def get_messages(room):
+@app.route('/chat_index', methods=['GET', 'POST'])
+def chat_index():
+    uid = uuid.uuid4()
+    session['uid'] = uid
+    """Login form to enter a room."""
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        room = form.room.data
+
+        session['name'] = name
+        session['room'] = room
+
+        return redirect(url_for('.chat'))
+    elif request.method == 'GET':
+        form.name.data = session.get('name', '')
+        form.room.data = session.get('room', '')
+    return render_template('chat_index.html', form=form)
+
+
+def get_messages(room, start, end):
     all_msg = []
-    while r.llen(room) != 0:
-        print(r.lpop(room))
-        if r.lpop(room):
-            msg = r.lpop(room).split(')(')
-            all_msg.append({'name': msg[1], 'msg': msg[0], 'time': msg[-1]})
+    for conv_msg in r.lrange(room, start, end):
+        msg = conv_msg.split('&&')
+        all_msg.append({'name': msg[1], 'msg': msg[0], 'time': msg[-1]})
     return all_msg
 
 
@@ -73,7 +92,7 @@ def change():
     room = session.get('room', '')
     new_room = request.form.get('room_ch')
     session['room'] = new_room
-    return render_template('chat.html', name=name, room=room, prev_msg=get_messages(room))
+    return render_template('chat.html', name=name, room=room, prev_msg=get_messages(room, 0, 20))
 
 
 @app.route('/chat')
@@ -83,17 +102,18 @@ def chat():
 
     if name == '' or room == '':
         return redirect(url_for('.index'))
-    return render_template('chat.html', name=name, room=room, prev_msg=get_messages(room))
+    return render_template('chat.html', name=name, room=room, prev_msg=get_messages(room, 0, 20))
 
 
 # When Client Enters
 @socketio.on('joined', namespace='/chat')
 def joined(message):
     room = session.get('room')
-    join_room(5)
+    
+    join_room(20)
     join_room(room)
-    emit('status', {'msg': session.get('name') + ' has entered the room'},
-         room=room, prev_msg=get_messages(room))
+    emit('status', {'msg': session.get('name') + ' has entered the room', "id": 'chat-' + str(room)},
+         room=room, prev_msg=get_messages(room, 0, 20))
 
 
 @socketio.on('text', namespace='/chat')
@@ -102,11 +122,11 @@ def text(message):
     name = session.get('name', '')
 
     time = ''.join(message['time'])
-    r.zadd('mykey', 'b', 2)
-    print(r.zcard('mykey'))
-    r.rpush(room, '%s)(%s)(%s' % (message['msg'], name, time))
 
-    emit('internal_msg', {'msg': message['msg'], 'id': '#chat', 'name': name}, room=room, name=name)
+    if r.rpush(room, '%s&&%s&&%s' % (message['msg'], name, time)) > 50:
+        r.lpop(room)
+
+    emit('internal_msg', {'msg': message['msg'], 'id': '#chat-' + str(room), 'name': name}, room=room, name=name)
 
 
 @socketio.on('exit_room', namespace='/chat')
