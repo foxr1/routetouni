61f53e4702c2ac8007d746a0fdd372ecec84fbd0
philipsolo <philipsolo4@gmail.com>
2020-12-18 12:03:30 +0200

    created user object for verification

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import os
-import uuid
-import datetime
-import flask
 import redis
 from flask import Flask, render_template, session, redirect, url_for, request, send_from_directory, Blueprint
 from flask_socketio import emit, join_room, leave_room, SocketIO
-import firebase_admin
-from firebase_admin import credentials, auth, exceptions
-from forms import LoginForm
-from flask_login import login_required, current_user
+from models import User, logout_user
 
 redis_host = os.environ.get('REDISHOST', 'localhost')
 redis_port = int(os.environ.get('REDISPORT', 6379))
@@ -22,64 +16,26 @@ socketio = SocketIO(app, async_mode=async_mode, logger=True, engineio_logger=Tru
 
 main = Blueprint('main', __name__)
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://route2uni-default-rtdb.firebaseio.com/'
-})
+test_user = User()
 
 
 @app.route('/sessionLogin', methods=['GET', 'POST'])
 def session_login():
-    id_token = request.args.get('idToken')
-    # Set session expiration to 5 days.
-    expires_in = datetime.timedelta(days=14)
-    try:
-        # Create the session cookie. This will also verify the ID token in the process.
-        # The session cookie will have the same claims as the ID token.
-        session_cookie = auth.create_session_cookie(id_token, expires_in=expires_in)
-        response = flask.jsonify({'status': 'success'})
-        # Set cookie policy for session cookie.
-        expires = datetime.datetime.now() + expires_in
-        response.set_cookie(
-            'session', session_cookie, expires=expires, httponly=True, secure=True)
-        return response
-    except exceptions.FirebaseError:
-        return flask.abort(401, 'Failed to create a session cookie')
+    response = test_user.login_user()
+    return response
 
 
-@app.route('/sessionLogout', methods=['GET','POST'])
+@app.route('/sessionLogout', methods=['GET', 'POST'])
 def session_logout():
-    print("Logging out")
-    session_cookie = flask.request.cookies.get('session')
-    try:
-        decoded_claims = auth.verify_session_cookie(session_cookie)
-        auth.revoke_refresh_tokens(decoded_claims['sub'])
-        response = flask.make_response(flask.redirect('/login'))
-        response.set_cookie('session', expires=0)
-        return response
-    except auth.InvalidSessionCookieError:
-        return flask.redirect('/login')
-
-
-def verify_token():
-    session_cookie = flask.request.cookies.get('session')
-    print(session_cookie)
-    if not session_cookie:
-        print("No Cookie")
-
-    else:
-        try:
-            decoded_claims = auth.verify_session_cookie(session_cookie, check_revoked=True)
-            print(decoded_claims)
-            return decoded_claims['uid']
-
-        except auth.InvalidSessionCookieError:
-            print("Verification Error")
+    response = logout_user()
+    print(response)
 
 
 @app.route('/')
 def index():
-    user = verify_token()
+    user = test_user.verify_user()
+    if test_user.email:
+        print(test_user.email,test_user.uid)
     return render_template("index.html", user=user)
 
 
@@ -104,38 +60,12 @@ def news_feed():
     return render_template("news_feed.html")
 
 
-@app.route('/chat_index', methods=['GET', 'POST'])
-@login_required
-def chat_index():
-    uid = uuid.uuid4()
-    session['uid'] = uid
-    """Login form to enter a room."""
-    form = LoginForm()
-
-    if form.validate_on_submit():
-        name = form.name.data
-        room = form.room.data
-
-        session['name'] = name
-        session['room'] = room
-
-        return redirect(url_for('.chat'))
-    elif request.method == 'GET':
-        form.name.data = session.get('name', '')
-        form.room.data = session.get('room', '')
-    return render_template('chat_index.html', form=form)
-
-
 def get_messages(room, start, end):
     all_msg = []
     for conv_msg in r.lrange(room, start, end):
         msg = conv_msg.split('&&')
         all_msg.append({'name': msg[1], 'msg': msg[0], 'time': msg[-1]})
     return all_msg
-
-
-def get_chats():
-    print('c')
 
 
 @app.route('/change', methods=['GET', 'POST'])
@@ -149,8 +79,8 @@ def change():
 
 @app.route('/chat')
 def chat():
-    name = session.get('name', '')
-    room = session.get('room', '')
+    name = test_user.uid
+    room = 5
 
     if name == '' or room == '':
         return redirect(url_for('.index'))
@@ -160,17 +90,17 @@ def chat():
 # When Client Enters
 @socketio.on('joined', namespace='/chat')
 def joined(message):
-    room = session.get('room')
+    room = 5
 
     join_room(room)
-    emit('status', {'msg': session.get('name') + ' has entered the room', "id": 'chat-' + str(room)},
+    emit('status', {'msg': test_user.uid + ' has entered the room', "id": 'chat-' + str(room)},
          room=room, prev_msg=get_messages(room, 0, 20))
 
 
 @socketio.on('text', namespace='/chat')
 def text(message):
-    room = session.get('room')
-    name = session.get('name', '')
+    room = 5
+    name = test_user.uid
 
     time = ''.join(message['time'])
 
@@ -182,9 +112,9 @@ def text(message):
 
 @socketio.on('exit_room', namespace='/chat')
 def exit_room(message):
-    room = session.get('room')
-    name = session.get('name', '')
-    now = datetime.datetime.now().replace(microsecond=0).time()
+    room = 5
+    name = test_user.uid
+
     leave_msg = name + ' has left the room.'
 
     leave_room(room)
