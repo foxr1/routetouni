0415b35c001b6c74277478e45acbf89c0f8037e8
foxr1 <ollie.fox5@gmail.com>
2021-01-08 12:15:15 +0000

    Fixed overwrite issue

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import os
-from datetime import date
+
 import flask
-from flask import Flask, render_template, session, redirect, url_for, send_from_directory, Blueprint
+import redis
+from flask import Flask, render_template, session, redirect, url_for, request, send_from_directory, Blueprint
 from flask_socketio import emit, join_room, leave_room, SocketIO
 from models import User
-from socket_manage import MessageManage
+
+
+redis_host = os.environ.get('REDISHOST', 'localhost')
+redis_port = int(os.environ.get('REDISPORT', 6379))
+r = redis.StrictRedis(host=redis_host, port=redis_port, charset="utf-8", decode_responses=True)
 
 async_mode = None
 app = Flask(__name__)
@@ -18,11 +23,11 @@ socketio = SocketIO(app, async_mode=async_mode, cors_allowed_origins=["https://e
 main = Blueprint('main', __name__)
 
 test_user = User()
-socket_man = MessageManage()
 
 
 @app.route('/sessionLogin', methods=['GET', 'POST'])
 def session_login():
+    print('logging in ')
     response = test_user.login_user()
     return response
 
@@ -37,7 +42,7 @@ def session_logout():
 def index():
     session_cookie = flask.request.cookies.get('session')
     if session_cookie:
-        user = test_user.verify_user()
+        user = test_user.verify_user(session_cookie)
     else:
         user = None
     return render_template("index.html", user=user)
@@ -64,47 +69,64 @@ def news_feed():
     return render_template("news_feed.html")
 
 
-@app.route('/chat')
-def chat():
-    test_user.verify_user()
-
-    if not test_user.verify_user():
-        return redirect(url_for('.index'))
-    else:
-        name = test_user.uid
-        room = 'room5'
-    return render_template('chat.html', name=name, room=room, prev_msg=socket_man.conv_dict(name))
-
-
 @app.route('/map')
 def map():
     return render_template("campus_map.html")
 
 
+def get_messages(room, start, end):
+    all_msg = []
+    for conv_msg in r.lrange(room, start, end):
+        msg = conv_msg.split('&&')
+        all_msg.append({'name': msg[1], 'msg': msg[0], 'time': msg[-1]})
+    return all_msg
+
+
+@app.route('/change', methods=['GET', 'POST'])
+def change():
+    name = session.get('name', '')
+    room = session.get('room', '')
+    new_room = request.form.get('room_ch')
+    session['room'] = new_room
+    return render_template('chat.html', name=name, room=room, prev_msg=get_messages(room, 0, 20))
+
+
+@app.route('/chat')
+def chat():
+    name = test_user.uid
+    room = 5
+
+    if name == '' or room == '':
+        return redirect(url_for('.index'))
+    return render_template('chat.html', name=name, room=room, prev_msg=get_messages(room, 0, 20))
+
+
 # When Client Enters
 @socketio.on('joined', namespace='/chat')
 def joined(message):
-    room = 'room5'
-    name = test_user.uid
+    room = 5
 
     join_room(room)
-    emit('status', {'msg': test_user.uid + ' has entered the room', "id": str(room)},
-         room=room, prev_msg=socket_man.conv_dict(name))
+    emit('status', {'msg': test_user.uid + ' has entered the room', "id": 'chat-' + str(room)},
+         room=room, prev_msg=get_messages(room, 0, 20))
 
 
 @socketio.on('text', namespace='/chat')
 def text(message):
-    room = 'room5'
+    room = 5
     name = test_user.uid
 
-    socket_man.add_message(room, message, name)
+    time = ''.join(message['time'])
 
-    emit('internal_msg', {'msg': message['msg'], 'id': str(room), 'name': name}, room=room, name=name)
+    if r.rpush(room, '%s&&%s&&%s' % (message['msg'], name, time)) > 50:
+        r.lpop(room)
+
+    emit('internal_msg', {'msg': message['msg'], 'id': '#chat-' + str(room), 'name': name}, room=room, name=name)
 
 
 @socketio.on('exit_room', namespace='/chat')
 def exit_room(message):
-    room = 'room5'
+    room = 5
     name = test_user.uid
 
     leave_msg = name + ' has left the room.'
