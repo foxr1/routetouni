55750c5121df85ff41674564f9b98c0f84b1dc12
b9022961 <b9022961@newcastle.ac.uk>
2021-01-27 10:24:40 +0000

    Completed some testing of socket manage

--- tests/test_socket_manage.py
+++ tests/test_socket_manage.py
@@ -8,3 +8,9 @@ def test_add_room():
     p = socket_man.r.xinfo_stream("Room_1")
     assert p
 
+def test_create_room():
+    socket_man.create_room("1234","Room_1","room_name","User_adding")
+
+    assert True
+
+    # create_room, check_user_in
