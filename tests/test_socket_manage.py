8421169419f60a96886a3289d8123f264b3a366e
Katie Fallon <b9018199>
2021-01-27 10:06:35 +0000

    Delete room test

--- tests/test_socket_manage.py
+++ tests/test_socket_manage.py
@@ -4,7 +4,15 @@ socket_man = MessageManage()
 
 
 def test_add_room():
-    socket_man.add_room("1234","Room_1","room_name","User_adding")
+    socket_man.add_room("1234", "Room_1", "room_name", "User_adding")
     p = socket_man.r.xinfo_stream("Room_1")
+    assert p
+
+
+def test_del_room():
+    socket_man.add_room("1234", "Room_1", "room_name", "User_adding")
+    socket_man.del_room("1234", "Room_1")
+    p = socket_man.check_user_in("1234", "Room_1")
+    print(p)
     assert p
 
