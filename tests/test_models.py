94bbb1dc6005a9bc96af9c6ac4b5ed2ff2c00f05
b8071126 <b8071126@newcastle.ac.uk>
2021-01-27 15:42:20 +0000

    Comments added for getting users test

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,14 +1,9 @@
 import models
-import firebase_admin
-from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://route2uni-default-rtdb.firebaseio.com/'
-})
 
-
+# Test that all users in the database can be retrieved
+# Expected outcome = True
 def test_get_all_users():
     assert models.get_all_users() == db.reference('users').get()
 
