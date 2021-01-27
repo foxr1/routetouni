65cf261e7f2af390f5836f07a3f19d0bcf7492d8
Katie Fallon <b9018199>
2021-01-27 11:17:11 +0000

    Fixed admin button

--- tests/test_socket_manage.py
+++ tests/test_socket_manage.py
@@ -5,10 +5,12 @@ socket_man.flush_db()
 
 #Test that a user can be added to a room
 #Expected outcome = True
+
 def test_add_room():
+    socket_man.flush_db()
     socket_man.add_room("1234", "Room_1", "room_name", "User_adding")
-    p = socket_man.r.xinfo_stream("Room_1")
-    assert p
+    new_room = socket_man.r.xinfo_stream("Room_1")
+    assert new_room['first-entry']['room_name'] == "Room_1"
 
 # Test that a user can delete a room
 # Check that the user is no longer in the room
@@ -16,23 +18,27 @@ def test_add_room():
 def test_del_room():
     socket_man.add_room("1234", "Room_1", "room_name", "User_adding")
     socket_man.del_room("1234", "Room_1")
-    p = socket_man.check_user_in("1234", "Room_1")
-    print(p)
-    assert p
+    del_room = socket_man.check_user_in("1234", "Room_1")
+    assert not del_room
 
 #Tests that a user can join a random room
 #Expected outcome = True
 def test_join_random():
     socket_man.join_random("1234", "Test Name")
     user_rooms = socket_man.r.hgetall("1234")
-    assert user_rooms
+    assert "Random_0" in user_rooms
 
 #Tests that a user can send a message in a room
 #Expected outcome = True
 def test_add_message():
     socket_man.add_room("1234", "Room_1", "room_name", "User_adding")
-    p= socket_man.r.xadd("Room_1",
+    socket_man.r.xadd("Room_1",
                        {"name": "1234", "msg": "Hello!", "time": "10:29", 'uid': "1234",
                         'picture': "", 'room_name': "Room_1"})
+    all_msg = []
+    for msg in socket_man.r.xrange("Room_1", "-", "+", count=20):
+        all_msg.append(msg[1])
 
-    assert p
+    assert all_msg[-1]['msg'] == "Hello!"
+
+
