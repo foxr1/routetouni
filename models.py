5f8f413220c3ab4814c3c68a00464017ea833d0e
philipsolo <philipsolo4@gmail.com>
2021-01-28 13:27:25 +0200

    service account key fix directory fix

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ import firebase_admin
 from firebase_admin import credentials, auth, exceptions
 from firebase_admin import db
 
-cred = credentials.Certificate("C:\\Users\\phili\PycharmProjects\\routetouni\\serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://route2uni-default-rtdb.firebaseio.com/'
 })
